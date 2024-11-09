572f9fab34d01e600b8ed269e53da00bf87e459d
Fallm3 <dieryfall@hotmail.com>
2024-11-09 01:05:52 -0500

    Last

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,8 @@ repositories {
 }
 
 dependencies {
+	implementation("com.graphql-java-kickstart:graphql-spring-boot-starter:12.0.0")
+	implementation("com.graphql-java-kickstart:graphql-java-tools:12.0.0")
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 	implementation("org.springframework.boot:spring-boot-starter-graphql")
 	implementation("org.springframework.boot:spring-boot-starter-security")
